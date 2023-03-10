b7355222fe7968d413ba07d03200c2c913a0c5ed
MohammedYasserr <bmbmyasser@gmail.com>
2023-03-10 17:05:21 +0200

    modyfing the view to rerurn the data format we desire

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,10 +4,13 @@ import json
 def api_home(request , *args, **kwargs):
     body = request.body 
     data ={}
+    print(request.GET) #It will return url query parameter
+    print(request.POST)
     try:
         data = json.loads(body) #This takes in String of json data --> returns a python dictionary
     except:
         pass
-    print(data.keys())
-    #print(body) )
+    
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
     return JsonResponse(data)
