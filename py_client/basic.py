d125ee69b017e56eb0a2b697e710d7de3401a1bf
MohammedYasserr <bmbmyasser@gmail.com>
2023-03-10 10:13:48 +0200

    Secret key production code

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,9 +2,9 @@ import requests
 
 
 #endpoint = "https://httpbin.org/anything" 
-endpoint = "http://127.0.0.1:8000/" 
+endpoint = "http://localhost:8000/" 
 
 
 get_response = requests.get(endpoint ,  json={"query" : "Hello world "}) 
-print(get_response) 
+print(get_response.text) 
 print(get_response.status_code)
